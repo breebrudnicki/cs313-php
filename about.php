047e14ebf940b92c700f0bbc72b96653d0562575
Bree Carrick <breecarrick@Brees-MacBook-Air.local>
2016-05-02 02:14:39 -0600

    All me new files for my initial home page, about me page, and assignments page. All necessary photos included.

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php
+$page = "about";
+?>
+<!doctype html>
+<html>
+
+<head>
+    <title> About Me </title>
+    <?php include "modules/links.html";?>
+</head>
+
+<body>
+    <header>
+        <a href="."><img src="images/headerimg.png" alt="Bree Carrick"></a>
+        <?php include "modules/nav.php"?>
+    </header>
+    <main>
+        <h2>About Me</h2>
+        <p>Hi there! My name is Bree Carrick and this is my website. My major is Web Design and Development with an emphasis in Development and a minor in Design. I'm from Portland Oregon and while I love going to school at BYU-Idaho, Oregon has my heart. Some of my favorite things to do include hiking, Pinterest, crafts, making spreadsheets, organizing things, and eating. My favorite food is cookie dough.</p>
+        <img class="about" src="images/bree.jpg" alt="Photo of Bree">
+        <img class="about" src="images/aboutpic.jpg" alt="Bree and Lance">
+    </main>
+    <?php include "modules/footer.html";
+    ?>
+</body>
+
+</html>
