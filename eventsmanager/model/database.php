3007d34968bb31cee9080c92312b0410c6cb84a0
Bree Carrick <breecarrick@Brees-MacBook-Air.local>
2016-05-28 12:31:19 -0600

    trouble shooting openshift mySQL connection

--- eventsmanager/model/database.php
+++ eventsmanager/model/database.php
@@ -14,7 +14,7 @@ function loadDatabase()
      if ($openShiftVar === null || $openShiftVar == "")
      {
           // Not in the openshift environment 
-          //echo "Using local credentials: "; 
+          echo "Using local credentials: "; 
           require("setLocalDatabaseCredentials.php");
           //echo "$dbHost $dbPort $dbUser $dbPassword";
      }
@@ -22,14 +22,14 @@ function loadDatabase()
      { 
           // In the openshift environment
           //echo "Using openshift credentials: ";
-
+          require("setRemoteDatabaseCredentials.php");
           $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
           $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
           $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
           $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
           $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
      } 
-     echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 
      //$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
