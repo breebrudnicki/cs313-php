a66663d7919f7c589c7e9cb19e61589fc4950af7
Bree Carrick <breecarrick@Brees-MacBook-Air.local>
2016-06-01 15:02:39 -0600

    changed the navigation bar logout button so it should work better

--- modules/nav.php
+++ modules/nav.php
@@ -1,33 +1,16 @@
 <nav>
-            <ul>
-                <li><a 
-                    <?php if ($page=="home") {
-                    echo 'class="active"';
-                }
-?> href="/index.php">Home</a></li>
-                <li><a 
-                        <?php if ($page=="about") {
-                    echo 'class="active"';
-                } ?> href="/about.php">About Me</a></li>
-                <li><a 
-                        <?php if ($page=="assignments") {
-                    echo 'class="active"';
-                }?> href="/assignments.php">Assignments</a></li>
-                <li><a 
-                        <?php if ($page=="eventsManager") {
-                    echo 'class="active"';
-                    }?> href="/eventsmanager/index.php">Events Manager</a></li>
-                <?php
+    <ul>
+        <li><a <?php if ($page=="home" ) { echo 'class="active"'; } ?> href="/index.php">Home</a></li>
+        <li><a <?php if ($page=="about" ) { echo 'class="active"'; } ?> href="/about.php">About Me</a></li>
+        <li><a <?php if ($page=="assignments" ) { echo 'class="active"'; }?> href="/assignments.php">Assignments</a></li>
+        <li><a <?php if ($page=="eventsManager" ) { echo 'class="active"'; }?> href="/eventsmanager/index.php">Events Manager</a></li>
+        <?php
                 if (!isset($_SESSION['loggedin'])) {
                     echo "<li class='right'><a href='/eventsmanager/index.php'>Login</a></li>";
                 } else {
-                    echo "<li class='right'><a href='?link=1'>Logout</a></li>";
-                     $link=$_GET['link'];
-                        if ($link == '1'){
-                            session_destroy();
-                        }
+                    echo "<li class='right'><a href='/eventsmanager/model/logout.php'>Logout</a></li>";
                 }
                 ?>
-            </ul>
-                        
-        </nav>
+    </ul>
+
+</nav>
