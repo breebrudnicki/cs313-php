7f0bead95c2d9ff7281e9c782e25b23159d3b637
Bree Carrick <breecarrick@Brees-MacBook-Air.local>
2016-06-01 12:31:42 -0600

    fixing errors

--- eventsmanager/index.php
+++ eventsmanager/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+require 'model/password.php';
 $page = "eventsManager";
 session_start();
 require_once 'model/sql.php';
